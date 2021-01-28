292d4595f2056c59e1622d5de64cfd92e331f14e
Prabhakar Kumar <prabhakar1069@gmail.com>
2021-01-28 23:08:28 +0530

    Changes routes.rb file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,18 @@
 class PostsController < ApplicationController
   def index
+    @post = Post.all
+    @user = User.all
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(params.require(:product).permit(:content))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,47 @@
 Rails.application.routes.draw do
-  root  'users#index'
-  get 'friendships/create'
-  get 'likes/create'
-  get 'comments/new'
-  get 'comments/create'
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'users/index'
-  get 'users/show'
+
+  devise_for :users
+
+  devise_scope :user do
+    authenticated :user do
+      root 'welcome#index', as: :authenticated_root
+    end
+  
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  resources :users, only: %i[index show] do
+    resources :friendships, only: %i[create]
+      
+  end
+  resources :posts, only: %i[index new create show] do
+    resources :likes, only: %i[create]
+  end
+
+  resources :comments, only: %i[new create index] do
+    resources :likes, only: %i[create]
+  end
+
+  
+  # get 'friendships/create'
+  # get 'likes/create'
+  # get 'comments/new'
+  # get 'comments/create'
+  # get 'comments/index'
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'posts/new'
+  # get 'posts/create'
+  # get 'users/index'
+  # get 'users/show'
   get 'list/friend'
   get '/search' => 'list#search', :as => 'search_list'
-  devise_for :users
+  
+  # root 'welcome#index'
+
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
